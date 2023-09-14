fix: calculate dr/dt analytically instead of numerical approximation

src/schrodinger/master.py
@@ -1,18 +1,26 @@
 import numpy as np
 
-from util.numeric import adams, insch, outsch
+from util.numeric import insch, outsch
+# handle the case if no compiled Fortran module is present
+from util.numeric.adams import _NO_FORTRAN
+if _NO_FORTRAN:
+    from util.numeric.adams import adams
+else:
+    from util.numeric.adams import adams_f as adams
 from util.math import count_nodes
 from schrodinger.coulomb.analytical import energy
 
 import matplotlib.pyplot as plt
 from typing import Union
 
+from multiprocessing import Pool
+
 
 def outer_classical_turning_point(V, E, l, r) -> int:
     return int(np.argmin(np.abs(E-(V+l*(l+1)/(2*r**2)))))
 
 
-def master(n, l, Z, M, V, r, t, charge=0,
+def master(n, l, Z, M, V, r, t, h, charge=0,
            order_adams: int = 7,
            order_insch: int = 7,
            E_guess: Union[float, str] = "auto",
@@ -22,11 +30,14 @@ def master(n, l, Z, M, V, r, t, charge=0,
     effective_charge = charge + 1  # an e- in distance feels the effective charge
 
     if E_guess == "auto":
-        E_guess = energy(n, Z, M)
+        E_guess = energy(n, Z, M) * .95  # inject artificial error
 
     # calculate/prepare outside the loop for speed improvements
-    b = np.diff(r, append=r[-1]) / np.diff(t, append=t[-1]+(t[-1]-t[-2]))
-    c = lambda E: -2 * b * (E - V - l * (l+1) / (2 * r ** 2))
+    #b = np.append([0], np.diff(r) / np.diff(t))
+    b = r[-1]/(np.exp(t[-1])-1)*np.exp(t)
+    # c = lambda E: -2 * b * (E - V - l * (l+1) / (2 * r ** 2))
+    def c(E):
+        return (-2 * b * (E - V - l * (l+1) / (2 * r ** 2)))
     G = np.zeros((len(b), 2, 2))
     G[:, 0, 1] = b
 
@@ -36,9 +47,10 @@ def master(n, l, Z, M, V, r, t, charge=0,
     E_u = np.inf  # highest energy with n_r nodes
     E_l = -np.inf   # lowest energy with n_r nodes
 
-    it = 1
-    while it <= max_number_of_iterations:
-        print("Iteration:", it)
+    it = 0
+    while it < max_number_of_iterations:
+        it += 1
+        # print("Iteration:", it)
 
         a_c = outer_classical_turning_point(V, E_guess, l, r)
 
@@ -53,16 +65,33 @@ def master(n, l, Z, M, V, r, t, charge=0,
 
         G[:, 1, 0] = c(E_guess)  # for inward integration the sign of b and therefore G changes!
 
-        y_out = adams.adams(order_adams, "out", y_start_out, G[:a_c+1], h)
-        y_in = adams.adams(order_adams, "in", y_start_in, -G[a_c:], h)
-        y_in[np.isclose(y_in, 0, atol=1e-10)] = 0
+        # # calculate in and out integration in the same step
+        # G_ = np.zeros((max(a_c+1, len(b)-a_c), 4, 4))
+        # G_[:a_c+1, :2, :2] = G[:a_c + 1]
+        # G_[-(len(G)-a_c):, 2:, 2:] = -G[a_c:]
+        # y_start = np.append(y_start_out, y_start_in, axis=1)
+
+        # with Pool(processes=2) as pool:
+        #     adams_out = pool.apply_async(adams, (order_adams, "out", y_start_out, G[:a_c+1], h))
+        #     adams_in = pool.apply_async(adams, (order_adams, "in", y_start_in, -G[a_c:], h))
+        #     pool.close()
+        #     pool.join()
+        #     y_out = adams_out.get()
+        #     y_in = adams_in.get()
+
+        # y_out = adams.adams_schrodinger(order_adams, "out", y_start_out, b[:a_c+1], c(E_guess)[:a_c+1], h)
+        # y_in = adams.adams_schrodinger(order_adams, "in", y_start_in, -b[a_c:], -c(E_guess)[a_c:], h)
+        y_out = adams(order_adams, "out", y_start_out, G[:a_c+1], h)
+        y_in = adams(order_adams, "in", y_start_in, -G[a_c:], h)
+        y_in[np.isclose(y_in, 0, atol=1e-15)] = 0
 
         y_in *= y_out[-1, 0]/y_in[0, 0]  # make R=y[:,0] continues
+        # y_out *= y_in[0, 0] / y_out[-1, 0]
 
         y = np.append(y_out, y_in[1:], axis=0)
 
         num_nodes = count_nodes(y[:, 0])
-        print(num_nodes)
+        # print(num_nodes)
 
         # fig, ax = plt.subplots(nrows=2, num=0)
         # ax[0].clear()
@@ -89,24 +118,24 @@ def master(n, l, Z, M, V, r, t, charge=0,
         if num_nodes < n_r:
             E_l = max(E_l, E_guess)  # keep track of the lower bound of energy (greatest energy with too few nodes)
 
-            if n_r - num_nodes > 5:  # if the guess is far off, increase the adjustment steps in energy
-                E_guess_new = E_guess * 0.5
-            else:
-                E_guess_new = E_guess * 0.8
+            # if n_r - num_nodes > 5:  # if the guess is far off, increase the adjustment steps in energy
+            #     E_guess_new = E_guess * 0.5
+            # else:
+            E_guess_new = E_guess * 0.9
         elif num_nodes > n_r:
             E_u = min(E_u, E_guess)  # keep track of the upper bound of energy (lowest energy with too many nodes)
-            if num_nodes - n_r > 5:  # if the guess is far off, increase the adjustment steps in energy
-                E_guess_new = E_guess * 2
-            else:
-                E_guess_new = E_guess * 1.2
+            # if num_nodes - n_r > 5:  # if the guess is far off, increase the adjustment steps in energy
+            #     E_guess_new = E_guess * 2
+            # else:
+            E_guess_new = E_guess * 1.1
         else:
             # check if the first derivative is continuous
             # if this is fulfilled, we found the eigenfunction and therefore the eigenenergy
-            print(y_out[-1, 1] - y_in[0, 1])
-            if np.isclose(y_out[-1, 1], y_in[0, 1], atol=1e-5):
+            # print(y_out[-1, 1] - y_in[0, 1])
+            if np.isclose(y_out[-1, 1] - y_in[0, 1], 0, atol=1e-10):
                 break
 
-            E_guess_new = E_guess + (y_out[-1, 1]-y_in[0, 1])*y_out[-1, 0] / np.trapz(y[:,0]**2 * b)
+            E_guess_new = E_guess + (y_out[-1, 1]-y_in[0, 1])*y_out[-1, 0] / (2*np.trapz(y[:,0]**2, x=r))
 
         if E_guess_new < E_l:
             E_guess_new = (E_guess+E_l)/2
@@ -114,38 +143,41 @@ def master(n, l, Z, M, V, r, t, charge=0,
             E_guess_new = (E_guess+E_u)/2
 
         E_guess = E_guess_new
-        print("new E_guess:", E_guess)
-        it += 1
+        # print("new E_guess:", E_guess)
 
     N = 1/np.sqrt(np.trapz(y[:,0]**2, x=r))
 
-    return N * y, E_guess, a_c
+    return N * y, E_guess, a_c, it
 
 
 if __name__ == "__main__":
-    Z = 1
+    from util.misc import find_suitable_number_of_integration_points
+    Z = 50
+    n, l = 4,1#10, 3
     M = np.inf
     mu = 1 / (1 + 1 / M)
 
-    N = 570
-    h = 0.025
-    r0 = 0.0005  # 0.0005
-    t = (np.arange(N) + 1) * h
+    # N = 570
+    h = 0.0005#0005 #0.00001
+    r0 = 0.0005
+    N = find_suitable_number_of_integration_points(Z, M, n, l, r0, h)
+    print(N)
+    t = np.arange(N) * h
     r = r0 * (np.exp(t) - 1)
+    r[np.isclose(r, 0, atol=1e-15)] = 1e-15
 
-    n, l = 15,10#10, 3
 
     V = - Z * 1/(1+1/M)/r
 
     import time
     t_start = time.time()
-    R, E, a_c = master(n, l, Z, M, V, r, t,
-                       order_adams = 9,
-                       order_insch = 5,
+    R, E, a_c, num_iteration = master(n, l, Z, M, V, r, t, h,
+                       order_adams = 11,
+                       order_insch = 7,
                        E_guess = "auto",
-                       max_number_of_iterations = 1000
+                       max_number_of_iterations = 50
                        )
-    print(f"finding eigenenergy and -function took {time.time()-t_start:.3f}s")
+    print(f"finding eigenenergy and -function took {time.time()-t_start:.3f}s and {num_iteration} iterations")
 
     import matplotlib.pyplot as plt
     from schrodinger.coulomb.analytical import radial_function, energy
@@ -161,10 +193,10 @@ if __name__ == "__main__":
     ax[0].plot(r, R_func, label="analytical")
     ax[1].plot(r[:-1], np.diff(R_func) / np.diff(r), label="analytical")
 
-    ax[0].plot(r[:a_c], R[:a_c, 0], ".", label="numerical out")
-    ax[1].plot(r[:a_c], R[:a_c, 1], ".", label="numerical out")
-    ax[0].plot(r[a_c:], R[a_c:, 0], ".", label="numerical in")
-    ax[1].plot(r[a_c:], R[a_c:, 1], ".", label="numerical in")
+    ax[0].plot(r[:a_c], R[:a_c, 0], "-", label="numerical out")
+    ax[1].plot(r[:a_c], R[:a_c, 1], "-", label="numerical out")
+    ax[0].plot(r[a_c:], R[a_c:, 0], "-", label="numerical in")
+    ax[1].plot(r[a_c:], R[a_c:, 1], "-", label="numerical in")
 
     ax[0].legend()
     # ax[0].set_xscale("log")

src/schrodinger/master2.py
@@ -32,7 +32,8 @@ def master(n, l, Z, M, V, r, t, charge=0,
         E_guess = energy(n, Z, M)
 
     # calculate/prepare outside the loop for speed improvements
-    b = np.diff(r, append=r[-1]) / np.diff(t, append=t[-1] + (t[-1] - t[-2]))
+    # b = np.diff(r, append=r[-1]) / np.diff(t, append=t[-1] + (t[-1] - t[-2]))
+    b = r[-1] / (np.exp(t[-1]) - 1) * np.exp(t)
     c = lambda E: -2 * b * (E - V - l * (l + 1) / (2 * r ** 2))
 
     n_r = n-l-1  # number of radial nodes
@@ -149,14 +150,18 @@ if __name__ == "__main__":
     M = np.inf
     mu = 1 / (1 + 1 / M)
 
-    n, l = 4,1
+    n, l = 4,1#4,1
+
 
     #N = 400
-    h = 0.02
-    r0 = 0.0005  # 0.0005
+    h = 0.0000005  #0.02
+    r0 = 0.1  # 0.0005
     N = find_suitable_number_of_integration_points(Z, M, n, l, r0, h)
-    t = (np.arange(N) + 1) * h
+    print(N)
+    # N = 1_000_000
+    t = np.arange(N) * h
     r = r0 * (np.exp(t) - 1)
+    r[np.isclose(r, 0, atol=1e-15)] = 1e-15
 
     V = - Z * 1/(1+1/M)/r
 
@@ -165,7 +170,7 @@ if __name__ == "__main__":
     R, E, a_c = master(n, l, Z, M, V, r, t,
                        order_adams = 9,
                        order_insch = 5,
-                       E_guess = -.1,#"auto",
+                       E_guess = "auto",
                        max_number_of_iterations = 1000
                        )
     print(f"finding eigenenergy and -function took {time.perf_counter()-t_start:.3f}s")

src/util/misc.py
@@ -20,7 +20,7 @@ def find_suitable_number_of_integration_points(Z, M, n, l,
     R = radial_function(n, l, r, Z, M)
     last_significant_r = r[np.max(np.argwhere(~np.isclose(R, 0, atol=1e-5)).reshape(-1))]
 
-    N_max = np.log(last_significant_r/r_0 + 1)/h - 1
+    N_max = int(np.floor(np.log(last_significant_r/r_0 + 1)/h)) - 1
 
-    return N_max + 10  # + 10 is just an arbitrary number to hold extra space
+    return int(N_max * 1.05)  # + 10 is just an arbitrary number to hold extra space
 

src/util/numeric/adams.py
@@ -141,10 +141,10 @@ if not _NO_FORTRAN:
 
 
 if __name__ == "__main__":
-    N = 100
-    h = 1  # 0.02
+    N = 10000
+    h = 0.0001  # 0.02
     r0 = 1  # 0.0005
-    t = np.arange(N)/h + 1
+    t = (np.arange(N) + 1) * h
     r = r0*(np.exp(t)-1)
 
     k = 13
@@ -170,15 +170,10 @@ if __name__ == "__main__":
     print(f"ellapsed time (adams): {t_end-t_start}s")
 
     t_start = time.perf_counter()
-    y2 = adams2(k, "out", y_start, G, h, AM_coeffs)
-    t_end = time.perf_counter()
-    print(f"ellapsed time (adams2): {t_end-t_start}s")
-
-    t_start = time.perf_counter()
-    y3 = adams_f(k, "out", y_start, G, h)
+    y2 = adams_f(k, "out", y_start, G, h)
     t_end = time.perf_counter()
     print(f"ellapsed time (adams_f): {t_end-t_start}s")
-    print(np.allclose(y2, y3))
+    print(np.allclose(y[~np.isnan(y)], y2[~np.isnan(y2)]))
 
     # print(y1)
     # print(y2)

src/util/numeric/insch.py
@@ -23,8 +23,8 @@ def insch(order, r, E, l, mu, effective_charge):
         return bks
 
     a = ak_list(order)
-    b = bk_list(order)
-    # b = [-lambda_] + [((sigma+k_)*(sigma-k_+1)-l*(l+1))/(2*k_)*a[k_-1] for k_ in range(1, order+1)]
+    #b = bk_list(order)
+    b = [-lambda_] + [((sigma+k_)*(sigma-k_+1)-l*(l+1))/(2*k_)*a[k_-1] for k_ in range(1, order+1)]
 
     R = np.power(r, sigma)*np.exp(-lambda_*r) * np.sum(np.array([a[k]/r**k for k in range(order+1)]), axis=0)
     Q = np.power(r, sigma)*np.exp(-lambda_*r) * np.sum(np.array([b[k]/r**k for k in range(order+1)]), axis=0)